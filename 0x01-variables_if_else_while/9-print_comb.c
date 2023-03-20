57eb2fcbb92ca7b1cc5bb83d498fea04940f6c3c
iWiafe <ismailawiafe@yahoo.com>
2023-03-19 17:50:56 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,11 @@ int main(void)
 	int d;
 
 	for (d = '0'; d <= '9'; d++)
-	{
-		putchar(d);
-	}
 	if (d != '9')
 	{
-		putchar(',', ' ');
+		putchar(d);
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);

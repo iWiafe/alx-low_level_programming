7fdacf83fe17de577cdc970fcc57661dc001258a
iWiafe <ismailawiafe@yahoo.com>
2023-03-21 18:21:40 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,7 @@ void print_alphabet_x10(void)
 	int j;
 
 	for (j = 1; i <= 10; i++)
-	{
-		for (j = 'a'; j <= 'z'; j++)
-		{
-			_putchar(j);
-		}
-		_putchar('\n');
-	}
+	for (j = 97; j <= 122; j++)
+	_putchar(j);
+	_putchar('\n');
 }

5d3289e03a3df086b9fa656275ee021d935fd511
iWiafe <ismailawiafe@yahoo.com>
2023-03-21 18:03:21 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10times the alphabets
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (j = 1; i <= 10; c++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}

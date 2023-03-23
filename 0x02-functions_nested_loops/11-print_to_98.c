5bc795149a03780925bc9e6351a6974c9d88d41f
iWiafe <ismailawiafe@yahoo.com>
2023-03-22 19:20:45 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_to_98 - Print natural numbers from n  upto 98
+ * @n: Input as an integer
+ * Return: Always (Success)
+ */
+void print_to_98(int n)
+{
+	int a, b;
+
+	if (n <= 98)
+	{
+		for (a = n; a <= 98; a++)
+		{
+			if (a != 98)
+			{
+				printf("%d, ", a);
+			}
+			else if (a == 98)
+			{
+				printf("%d\n", a);
+			}
+		}
+
+	else if (n >= 98)
+		for (b = n; b >= 98; b++)
+		{
+			if (b != 98)
+			{
+				printf("%d, ", b);
+			}
+			else if (b == 98)
+			{
+				printf("%d\n", b);
+			}
+		}
+	}
+}

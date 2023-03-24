cfef0a465de0c3daf51ff40eb3bf31c150f54dc0
iWiafe <ismailawiafe@yahoo.com>
2023-03-24 16:36:20 -0700

    Print diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,15 +7,25 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
 
-	if (n > 0)
-	{
-	for (i = 1; i <= n; i++)
-	{
-		_putchar('\\');
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+		}
+		_putchar('\n');
 	}
-	}
-	_putchar('\n');
 }
 

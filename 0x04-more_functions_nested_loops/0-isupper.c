101d22bcd2ac17781187c58e9f1dce1676d7e5ab
iWiafe <ismailawiafe@yahoo.com>
2023-03-23 19:52:04 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,15 +7,9 @@
  * Return: 1 for uppercase and 0 for the others
  */
 int _isupper(int c)
-{
-	int c;
-
+(
 	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
+	return (1);
 	else
-	{
-		return (0);
-	}
+	return (0);
 }

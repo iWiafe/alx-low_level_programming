f3dace626fdada4697582b8873f085ac99e3d875
iWiafe <ismailawiafe@yahoo.com>
2023-03-24 18:07:04 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,31 +11,3 @@
 int main(void)
 {
 	int i;
-
-	for (i = 1; i <= 100; 1++)
-	{
-		if (i % 3 == 0 && i % 5 != 0)
-		{
-			printf(" Fizz");
-		}
-		else if (i % 5 == 0 && i % 3 != 0)
-		{
-			printf(" Buzz");
-		}
-		else if (i % 3 == 0 && i % 5 != 0)
-		{
-			printf(" FizzBuzz");
-		}
-		else if (i == 1)
-		{
-			printf("%d", i);
-		}
-		else
-		{
-			printf(" %d", i);
-		}
-	}
-	printf("\n");
-	return (0);
-}
-

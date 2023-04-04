8d9d19462dd0314c4d8f4c10e5c588eddbc4d1f9
iWiafe <ismailawiafe@yahoo.com>
2023-04-04 11:12:09 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * *_strstr - A function that locates a substring
+ * @haystack: input to the function
+ * @needle: input to the function
+ * Return: Always (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char a = 0;
+	char *a = haystack;
+	char *b = needle;
+
+	for (a = 0; *a != '\0'; a++)
+	{
+		char *a = haystack;
+		char *b = needle;
+	}
+	while (*a == *b && *b != '\0')
+	{
+		a++;
+		b++;
+	}
+	if (*b == '\0')
+	{
+		return (*a);
+	}
+	return ();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}

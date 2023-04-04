f0f66ab0bea1a236398c3fd166c38dbf868dc21e
iWiafe <ismailawiafe@yahoo.com>
2023-04-04 11:35:12 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,15 +14,16 @@ char *_strstr(char *haystack, char *needle)
 	{
 		char *a = haystack;
 		char *b = needle;
-	}
-	while (*a == *b && *b != '\0')
-	{
-		a++;
-		b++;
-	}
-	if (*b == '\0')
-	{
-		return (haystack);
+
+		while (*a == *b && *b != '\0')
+		{
+			a++;
+			b++;
+		}
+		if (*b == '\0')
+		{
+			return (haystack);
+		}
 	}
 	return (0);
 }

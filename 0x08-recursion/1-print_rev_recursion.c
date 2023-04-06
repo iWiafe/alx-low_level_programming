a150e7fe5362e672c22fff2ffcdead2fad04cd0f
iWiafe <ismailawiafe@yahoo.com>
2023-04-05 17:51:23 -0700

    Why is it so important to dream? Because, in my dreams we are together

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,12 +9,9 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return (*s);
+		return;
 	}
-	else
-	{
-		s++;
-		_print_rev_recursion(s);
-		_putchar(*s);
-	}
+	s++;
+	_print_rev_recursion(s);
+	_putchar(*s);
 }

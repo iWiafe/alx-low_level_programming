9ef8dca08e9e3266d54cc9686368c7aac2f2749a
iWiafe <ismailawiafe@yahoo.com>
2023-04-14 12:11:02 -0700

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,6 +2,26 @@
 #include <stdlib.h>
 
 /**
+ * *_memset - A function that fills memory with a constant byte
+ * @s: starting address of memory
+ * @b: the desired byte
+ * @n: number of bytes of the memory area
+ * Return: a pointer to the memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int a = 0;
+
+	for (a = 0; n > 0; a++)
+	{
+		s[a] = b;
+		n--;
+	}
+	return (s);
+}
+
+
+/**
  * *_calloc - a function that allocates memory for an array, using malloc
  * @nmemb: number of element
  * @size: size of the element in byte

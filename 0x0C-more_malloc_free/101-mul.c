ecd7aaa9d72f77a42a6791b026ce768237de02eb
iWiafe <ismailawiafe@yahoo.com>
2023-04-14 16:13:36 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define EER_MSG "Error"
+
 /**
  * is_digit - a function which checks if a string contains a non-digit char
  * @s: string to be checked
@@ -20,7 +22,6 @@ int is_digit(char *s)
 	return (1);
 }
 
-
 /**
  * _strlen - A function that returns the length of a string
  * @s: string
@@ -38,11 +39,10 @@ int _strlen(char *s)
 	return (longi);
 }
 
-
 /**
  * errors - handles errors
  */
-void errors()
+void errors(void)
 {
 	printf("Error\n");
 	exit(98);
@@ -67,46 +67,29 @@ int main(int argc, char *argv[])
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
 	if (!result)
-	{
 		return (1);
-	}
 	for (a = 0; a <= len1 + len2; a++)
-	{
 		result[a] = 0;
-	}
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
-		{
 			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
-		}
 		if (carry > 0)
-		{
 			result[len1 + len2 + 1] += carry;
-		}
 	}
 	for (a = 0; a < len - 1; a++)
-	{
 		if (result[a])
-		{
 			b = 1;
-		}
 		if (b)
-		{
 			_putchar(result[a] + '0');
-		}
-	}
 	if (!b)
-	{
 		_putchar('0');
-	}
 	_putchar('\n');
 	free(result);
 	return (0);
 }
-

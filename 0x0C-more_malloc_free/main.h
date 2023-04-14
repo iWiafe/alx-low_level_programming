3b1249b2843593c2bc763c5571db971e4de97364
iWiafe <ismailawiafe@yahoo.com>
2023-04-14 15:59:48 -0700

    main header

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,4 +9,5 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *_memset(char *s, char b, unsigned int n);
 int _strlen(char *s);
+int is_digit(char *s);
 #endif

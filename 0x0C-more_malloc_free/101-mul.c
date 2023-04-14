cbf9d29a05229e1260901d5736a058ab877de651
iWiafe <ismailawiafe@yahoo.com>
2023-04-14 16:47:04 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -30,14 +30,13 @@ int is_digit(char *s)
  */
 int _strlen(char *s)
 {
-	int longi = 0;
+	int a = 0;
 
-	while (*s != '\0')
+	while (s[a] != '\0')
 	{
-		longi++;
-		s++;
+		a++;
 	}
-	return (longi);
+	return (a);
 }
 
 /**

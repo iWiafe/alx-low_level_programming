778299a517115b0d98d14bf3c2d4dacf924d3f36
iWiafe <ismailawiafe@yahoo.com>
2023-04-14 17:20:16 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -56,40 +56,44 @@ void errors(void)
  */
 int main(int argc, char *argv[])
 {
-	char *s1, *s2;
-	int len1, len2, len, a, carry, digit1, digit2, *result, b = 0;
+	char *num1, *num2;
+	int len1, len2, len, a, carry, digit1, digit2, *mul, b = 0;
 
-	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+	num1 = argv[1], num2 = argv[2];
+	if (argc != 3 || !is_digit(num1) || !is_digit(num2))
 		errors();
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
+	len1 = _strlen(num1);
+	len2 = _strlen(num2);
 	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len);
-	if (!result)
+	mul = malloc(sizeof(int) * len);
+	if (!mul)
 		return (1);
 	for (a = 0; a <= len1 + len2; a++)
-		result[a] = 0;
+		mul[a] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		digit1 = s1[len1] - '0';
+		digit1 = num1[len1] - '0';
 		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
+		for (len2 = _strlen(num2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = num2[len2] - '0';
+			carry += mul[len1 + len2 + 1] + (digit1 * digit2);
+			mul[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
+		}
 		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
+			mul[len1 + len2 + 1] += carry;
 	}
 	for (a = 0; a < len - 1; a++)
-		if (result[a])
+	{
+		if (mul[a])
 			b = 1;
 		if (b)
-			_putchar(result[a] + '0');
+			_putchar(mul[a] + '0');
+	}
 	if (!b)
 		_putchar('0');
 	_putchar('\n');
-	free(result);
+	free(mul);
 	return (0);
 }

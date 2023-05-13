f6c80ec9b915b64b2ce4b6300c3caf92a55e72b4
iWiafe <ismailawiafe@yahoo.com>
2023-05-12 17:43:57 -0700

    elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,5 +1,10 @@
 #include <elf.h>
-#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
@@ -56,7 +61,9 @@ void print_magic(unsigned char *e_ident)
 			printf("\n");
 		}
 		else
+		{
 			printf(" ");
+		}
 	}
 }
 
@@ -72,20 +79,14 @@ void print_class(unsigned char *e_ident)
 	switch (e_ident[EI_CLASS])
 	{
 	case ELFCLASSNONE:
-	{
 		printf("none\n");
 		break;
-	}
 	case ELFCLASS32:
-	{
 		printf("ELF32\n");
 		break;
-	}
 	case ELFCLASS64:
-	{
 		printf("ELF64\n");
 		break;
-	}
 	default:
 		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 	}
@@ -103,20 +104,14 @@ void print_data(unsigned char *e_ident)
 	switch (e_ident[EI_DATA])
 	{
 	case ELFDATANONE:
-	{
 		printf("none\n");
 		break;
-	}
 	case ELFDATA2LSB:
-	{
 		printf("2's complement, little endian\n");
 		break;
-	}
 	case ELFDATA2MSB:
-	{
 		printf("2's complement, big endian\n");
 		break;
-	}
 	default:
 		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 	}
@@ -135,12 +130,9 @@ void print_version(unsigned char *e_ident)
 	switch (e_ident[EI_VERSION])
 	{
 	case EV_CURRENT:
-	{
 		printf(" (current)\n");
 		break;
-	}
 	default:
-	{
 		printf("\n");
 		break;
 	}
@@ -157,7 +149,7 @@ void print_osabi(unsigned char *e_ident)
 
 	switch (e_ident[EI_OSABI])
 	{
-	case ELFOSABI_NONE:{
+	case ELFOSABI_NONE:
 		printf("UNIX - System V\n");
 		break;
 	case ELFOSABI_HPUX:
